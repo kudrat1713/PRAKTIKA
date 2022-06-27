2ef21fc749bd428babac0330383f0c7f7bbfbd74
kudrat1713 <102958164+kudrat1713@users.noreply.github.com>
2022-06-28 00:23:23 +0300

    Initial commit

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+    [SerializeField] float screenWidthInUnits = 16f;
+
+   
+    void Start()
+    {
+
+    }
+
+    
+    void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+       
+        Vector2 paddlePos = new Vector3(mousePos.x, transform.position.y, transform.position.z);
+        paddlePos.x = Mathf.Clamp(paddlePos.x, minX, maxX);
+        transform.position = paddlePos;
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour
+{
+    int currentSceneIndex;
+    
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+        FindObjectOfType<GameStatus>().ResetGame();
+    }    
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
+
+
